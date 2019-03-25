2120993499e3a4ff0866de7d360927dabce1b684
Trent Williams <proteinshakedev@gmail.com>
2019-03-25 18:34:16 -0400

    Full impl of Questionnaire

---
+++ src/dev/warrensnipes/cashregisterserver/utils/Questionaire.java
@@ -0,0 +1,41 @@
+package dev.warrensnipes.cashregisterserver.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Questionaire {
+
+    private final Map<Integer, String> questions;
+    private Map<Integer, String> answers;
+    private int count;
+
+    public Questionaire(Map<Integer, String> questions) {
+        this.questions = questions;
+        this.answers = new HashMap<>(questions.size());
+    }
+
+    public void addAnswer(String answer) {
+        answers.put(count, answer);
+        count++;
+    }
+
+    public void putAnswer(int question, String answer) {
+        answers.put(question, answer);
+    }
+
+    public String getQuestion() {
+        return questions.get(count);
+    }
+
+    public String getQuestion(int questionNumber) {
+        if (questionNumber >= questions.size()) {
+            throw new RuntimeException("You can't get a question that's out of the HashMap's size.");
+        }
+        return questions.get(questionNumber);
+    }
+
+    public void reset() {
+        this.count = 0;
+        answers.clear();
+    }
+}
