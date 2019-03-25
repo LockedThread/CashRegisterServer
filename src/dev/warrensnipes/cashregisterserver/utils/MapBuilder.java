7a5e6bd32da7d65037fd13a7391d993514851f13
Trent Williams <proteinshakedev@gmail.com>
2019-03-25 18:14:01 -0400

    MapBuilder class for easily creation of maps

---
+++ src/dev/warrensnipes/cashregisterserver/utils/MapBuilder.java
@@ -0,0 +1,27 @@
+package dev.warrensnipes.cashregisterserver.utils;
+
+import java.util.HashMap;
+
+public class MapBuilder<K, V> {
+
+    private HashMap<K, V> hashMap;
+
+    public MapBuilder() {
+        this.hashMap = new HashMap<>();
+    }
+
+    public MapBuilder<K, V> put(K k, V v) {
+        hashMap.put(k, v);
+        return this;
+    }
+
+    public V get(K k) {
+        return hashMap.get(k);
+    }
+
+    public HashMap<K, V> build() {
+        return hashMap;
+    }
+
+
+}
